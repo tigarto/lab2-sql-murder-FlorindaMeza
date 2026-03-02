c835b34bc0f945f82fa09acbddbae0be76cd53e7
tigarto <helpu1409@gmail.com>
2026-03-02 09:18:40 -0500

    Proceso de recopilacion de pruebas

--- consultas/respuestas.sql
+++ consultas/respuestas.sql
@@ -1,1 +1,17 @@
--- To Do
+
+-- SELECT * FROM crime_scene_report; 
+-- XXX
+
+-----------------------------------------------------------------------
+-- 1
+-- Conclusion: XXXXX
+-- Captura: paso1_repote.png
+
+SELECT * FROM crime_scene_report 
+WHERE date = 20180115 AND city = 'SQL City' AND type = 'murder';
+-----------------------------------------------------------------------
+-- 2
+
+
+------------------------------------------------------------------------
+
